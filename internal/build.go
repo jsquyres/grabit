a11f9b1d7afa4a08b46ed422d78ce04f80b6ac0e
Uzair Seyal <37130007+777Denoiser@users.noreply.github.com>
2024-10-23 22:20:15 -0400

    Update build.go

--- internal/build.go
+++ internal/build.go
@@ -1,11 +1,6 @@
 // Copyright (c) 2023 Cisco Systems, Inc. and its affiliates
 // All rights reserved.
 
-// This Go package defines versioning information for the application,
-// including the current version, commit identifier, and build date.
-// These variables can be used for displaying version information
-// in the application or for logging purposes.
-
 package internal
 
 var (
