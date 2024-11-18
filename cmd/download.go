e28f6ce7c515aaff2a100830c3f26c7c99dceaca
Amin assaid <146803615+eeasaa01@users.noreply.github.com>
2024-11-18 15:14:23 -0500

    Delete cmd/6ae8a75555209fd6c44157c0aed8016e763ff435a19cf186f76863140143ff72

--- cmd/download.go
+++ cmd/download.go
@@ -1,10 +1,10 @@
 // Copyright (c) 2023 Cisco Systems, Inc. and its affiliates
 // All rights reserved.
+
 package cmd
 
 import (
 	"github.com/cisco-open/grabit/internal"
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
@@ -15,12 +15,10 @@ func addDownload(cmd *cobra.Command) {
 		Args:  cobra.NoArgs,
 		RunE:  runFetch,
 	}
-
 	downloadCmd.Flags().String("dir", ".", "Target directory where to store the files")
 	downloadCmd.Flags().StringArray("tag", []string{}, "Only download the resources with the given tag")
 	downloadCmd.Flags().StringArray("notag", []string{}, "Only download the resources without the given tag")
 	downloadCmd.Flags().String("perm", "", "Optional permissions for the downloaded files (e.g. '644')")
-
 	cmd.AddCommand(downloadCmd)
 }
 
@@ -29,37 +27,29 @@ func runFetch(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-
 	lock, err := internal.NewLock(lockFile, false)
 	if err != nil {
 		return err
 	}
-
 	dir, err := cmd.Flags().GetString("dir")
 	if err != nil {
 		return err
 	}
-
 	tags, err := cmd.Flags().GetStringArray("tag")
 	if err != nil {
 		return err
 	}
-
 	notags, err := cmd.Flags().GetStringArray("notag")
 	if err != nil {
 		return err
 	}
-
 	perm, err := cmd.Flags().GetString("perm")
 	if err != nil {
 		return err
 	}
-
-	// Download resources
-	if err := lock.Download(dir, tags, notags, perm); err != nil {
+	err = lock.Download(dir, tags, notags, perm)
+	if err != nil {
 		return err
 	}
-
-	log.Debug().Msg("Download completed successfully")
 	return nil
 }
